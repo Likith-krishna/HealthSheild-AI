5c71b995702740d2b9b4b8a52f3e60f83397bbb7
LIKITH <likith@localhost.local>
2026-06-18 00:30:21 +0530

    chore: clean up temporary debugging and refactoring scripts

--- fix_tGlobal.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = 'src/components/HealthCollectionForm.tsx';
-let code = fs.readFileSync(path, 'utf8');
-
-// Replace all usages of tGlobal with t
-code = code.replace(/\btGlobal\b/g, 't');
-
-fs.writeFileSync(path, code);
-console.log("Replaced tGlobal with t.");
--- test_babel.js
+++
@@ -1,5 +0,0 @@
-import { parse } from '@babel/parser';
-import traverse from '@babel/traverse';
-import generate from '@babel/generator';
-
-console.log("Babel is available!");
